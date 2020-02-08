da8ca41eb37b479d60b586448217ff70cc89c580
fahdikrie <fahdiaf@gmail.com>
2020-02-08 16:49:16 +0700

    finished adding comments feature

--- blogpage/forms.py
+++ blogpage/forms.py
@@ -3,6 +3,30 @@ from . import models
 
 
 class CommentForm(forms.ModelForm):
+    name = forms.CharField(label='name',widget=forms.TextInput(
+        attrs={
+            'class' : 'form-control form-control',
+            'placeholder' : 'name...',
+            'style' : 'width: 50vw;'
+        }, 
+    ))
+
+    email = forms.CharField(label='email',widget=forms.TextInput(
+        attrs={
+            'class' : 'form-control form-control',
+            'placeholder' : 'email...',
+            'style' : 'width: 50vw;'
+        }, 
+    ))
+
+    body = forms.CharField(label='tell me something good!',widget=forms.Textarea(
+        attrs={
+            'class' : 'form-control form-control',
+            'placeholder' : 'write your comment here...',
+            'style' : 'height: 10vh; overflow: auto!important; width: 50vw;',
+        }, 
+    ))
+
     class Meta:
         model = models.Comment
         fields = ('name', 'email', 'body')
--- blogpage/views.py
+++ blogpage/views.py
@@ -7,12 +7,24 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
 
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'singlepost.html'
+# class PostDetail(generic.DetailView):
+#     model = Post
+#     template_name = 'singlepost.html'
 
 def post_detail(request, slug):
     template_name = 'singlepost.html'
+
+    # model = Post.objects.all().values()
+    # objIndex = None
+    # identifiedSlug = str(slug)
+
+    # for i in model:
+    #     if model[i]['slug'] == identifiedSlug:
+    #         objIndex = i
+    #         break
+
+    # singlePosts = model[objIndex]
+
     post = get_object_or_404(Post, slug=slug)
     comments = post.comments.filter(active=True)
     new_comment = None
@@ -21,11 +33,8 @@ def post_detail(request, slug):
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
 
-            # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
             new_comment.post = post
-            # Save the comment to the database
             new_comment.save()
     else:
         comment_form = CommentForm()
